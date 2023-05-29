73833b1ab52282b038e66d113f1bdab85f381480
frankolms <frankolms@gmail.com>
2023-05-28 20:12:05 -0400

    completed styling, added functionality to capitalize user's name when signing up

--- public/js/login.js
+++ public/js/login.js
@@ -23,7 +23,11 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector("#name-signup").value.trim();
+  const nameNotCapitalized = document
+    .querySelector("#name-signup")
+    .value.trim();
+  const name =
+    nameNotCapitalized.charAt(0).toUpperCase() + nameNotCapitalized.slice(1);
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
